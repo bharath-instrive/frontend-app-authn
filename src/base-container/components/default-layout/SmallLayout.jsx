1cc9b82bfa16634f88e0d40a865c0d90d339f09e
dhanasra <dhana.spiderlingz@gmail.com>
2026-02-10 12:58:35 +0530

    ui updated

--- src/base-container/components/default-layout/SmallLayout.jsx
+++ src/base-container/components/default-layout/SmallLayout.jsx
@@ -12,7 +12,7 @@ const SmallLayout = () => {
 
   return (
     <span className="bg-primary-400 w-100">
-      <div className="col-md-12 small-screen-top-stripe" />
+      {/* <div className="col-md-12 small-screen-top-stripe" /> */}
       <div>
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
           <Image className="logo-small" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
