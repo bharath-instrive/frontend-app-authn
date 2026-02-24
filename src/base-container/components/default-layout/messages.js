3c5fc788fa4e6f4ee657a872f664901a09ecb755
Bharathraj E <85331412+bharath-instrive@users.noreply.github.com>
2026-02-24 18:15:24 +0530

    text re update

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'start.learning': {
     id: 'start.learning',
-    defaultMessage: 'Manage & Grow Your Presence',
+    defaultMessage: 'Manage & Grow Your Presence on',
     description: 'Header text for logistration MFE pages',
   },
   'with.site.name': {
     id: 'with.site.name',
-    defaultMessage: 'on {siteName}',
+    defaultMessage: 'LexconX',
     description: 'Header text with site name for logistration MFE pages',
   },
 });
